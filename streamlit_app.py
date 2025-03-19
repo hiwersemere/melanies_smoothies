fd2f00584c0b6170a558364b93afdf3de58e6a2a
hiwersemere <77249161+hiwersemere@users.noreply.github.com>
2025-03-19 17:22:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,15 +44,14 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen  + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
         
-        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
         
         # st.text(smoothiefroot_response)
 
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     
     # st.write(ingredients_string)
@@ -70,20 +69,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-        
-# # Import python packages
-# import streamlit as st
-# from snowflake.snowpark.functions import col
-# import requests
-
-
-# helpful_links = [
-#     "https://docs.streamlit.io",
-#     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-#     "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-#     "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-# ]
-
+ 
 # # Write directly to the app
 # st.title(":cup_with_straw: Custom Your Smoothie:cup_with_straw:")
 # st.write("Choose the fruits you want in your custom Smoothie!:")
