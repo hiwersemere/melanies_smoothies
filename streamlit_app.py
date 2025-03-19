18cd108abcb5b8a1ce96afbdc0a6ef2e6b573a4d
hiwersemere <77249161+hiwersemere@users.noreply.github.com>
2025-03-19 14:51:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredients_list:
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
             
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
-        st.text(smoothiefroot_response)
+        st.text(smoothiefroot_response.json())
 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
