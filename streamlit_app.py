ca6016a51bcd32669e834c3f6e208df1d4165def
hiwersemere <77249161+hiwersemere@users.noreply.github.com>
2025-03-19 14:44:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredients_list:
         fruityvice_response = requests.get("https://my.fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
             
-        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
-        # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     
     # st.write(ingredients_string)
