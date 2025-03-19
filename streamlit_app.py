c844a7daa15e7bb3227b47af4504f68bfc78cfcf
hiwersemere <77249161+hiwersemere@users.noreply.github.com>
2025-03-19 14:46:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if ingredients_list:
     
     # st.write(my_insert_stmt)
     time_to_insert = st.button("Submit Order")
-    # st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
 
     # st.stop()
     
