e155f46ed7fd9d3bd5e522bd65d0927a45cd8596
hiwersemere <77249161+hiwersemere@users.noreply.github.com>
2025-03-19 17:26:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,21 +36,36 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:'
 )
 if ingredients_list:
     ingredients_string = ''
-    
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        st.subheader(fruit_chosen  + ' Nutrition Information')
-        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+
+    search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+    st.subheader(fruit_chosen + ' Nutrition Information')
+    fruitvvice_response = requests.get("https://fruitvvice.com/api/fruit/" + search_on)
+    fv_df = st.dataframe(data=fruitvvice_response.json(), use_container_width=True)
+
+# st.write(ingredients_string)
+    
+# if ingredients_list:
+#     ingredients_string = ''
+    
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen + ' '
         
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+#         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+#         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+#         st.subheader(fruit_chosen  + ' Nutrition Information')
+#         # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+#         # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
         
-        # st.text(smoothiefroot_response)
+#         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+#         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+        
+#         # st.text(smoothiefroot_response)
 
 
     
